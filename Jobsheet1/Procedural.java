de82e66b9f51ca6530e7fb3db72f3c5c65efef21
vicodwijaya-ai <vicodwijaya@gmail.com>
2026-08-28 09:34:05 +0700

    Procedural

---
+++ Jobsheet1/Procedural.java
@@ -0,0 +1,22 @@
+public class Procedural {
+    public static void main(String[] args) {
+        String itemName1 = "Coffee";
+        float price1 = 120000.0f;
+        int quantity1 = 3;
+
+        String itemName2 = "Sugar";
+        float price2 = 30000.0f;
+        int quantity2 = 1;
+
+        String itemName3 = "tea";
+        float price3 = 50000.0f;
+        int quantity3 = 2;
+
+        System.out.println(itemName1 + ": " + total(price1, quantity1));
+        System.out.println(itemName2 + ": " + total(price2, quantity2));
+
+    }
+    static float total(float price, int quantity) {
+        return price * quantity;
+    }
+}
